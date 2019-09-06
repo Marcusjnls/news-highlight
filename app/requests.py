2c4028e4f6a5559196a43c9de250e0707ff321fc
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-09-06 15:28:30 +0300

    Add get_sources method in requests.py file to feth API request

--- app/requests.py
+++ app/requests.py
@@ -1,13 +1,28 @@
 import urllib.request,json
 from .models import Sources
 
-#getting the api key
 api_key = None
-
-#getting the news base url
-base_url = None
+base_url = None #set variables to none
 
 def configure_request(app):
 	global api_key,base_url
 	api_key = app.config['NEWS_API_KEY']
 	base_url = app.config['NEWS_SOURCES_BASE_URL']
+
+def get_sources():
+	'''
+	Function that gets the json response to our url request
+	'''
+	get_sources_url = base_url.format(api_key)
+
+	with urllib.request.urlopen(get_sources_url) as url:
+		get_sources_data = url.read()
+		get_sources_response = json.loads(get_sources_data)
+
+		news_results = None
+
+		if get_sources_response['results']:
+			sources_results_list = get_sources_response['results']
+			sources_results = process_results(sources_results_list)
+
+	return news_results 
