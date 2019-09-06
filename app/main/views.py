673f21691adb37cc460ce14b42035fa2e2fe0534
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-09-06 15:42:11 +0300

    Add view articles function that opens articles page

--- app/main/views.py
+++ app/main/views.py
@@ -6,7 +6,7 @@ from ..models import Sources
 @main.route('/')
 def index():
 	"""
-	view root page function that returns the index page and its data
+	view home page function that returns the index page and its data
 	"""
 	sources = get_sources('business')
 	sports_sources = get_sources('sports')
@@ -15,3 +15,13 @@ def index():
 	title = "News Highlighter"
 
 	return render_template('index.html',title = title, sources = sources,sports_sources = sports_sources,technology_sources = technology_sources,entertainment_sources = entertainment_sources)
+
+@main.route('/sources/<id>')
+def articles(id):
+	"""
+	view articles page
+	"""
+	articles = get_articles(id)
+	title = f'NH | {id}'
+
+	return render_template('articles.html',title= title,articles = articles)
